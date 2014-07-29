69d503519667a45649b5105bf3cfe6a940d945d3
Emmanuel Mathot <emmanuel.mathot@skynet.be>
2014-07-29 10:11:41 +0200

    EO Metadata

---
+++ Terradue.Metadata.EarthObservation/Terradue/Metadata/EarthObservation/Namespaces.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Terradue.Metadata.EarthObservation {
+    public class EONamespaces {
+
+        public static NameValueCollection TypeNamespaces {
+            get {
+                NameValueCollection nvc = new NameValueCollection();
+                nvc.Set("http://www.opengis.net/sar/2.1", "sar21");
+                nvc.Set("http://www.opengis.net/eop/2.1", "eop21");
+                nvc.Set("http://www.opengis.net/opt/2.1", "opt21");
+                nvc.Set("http://www.opengis.net/sar/2.0", "sar20");
+                nvc.Set("http://www.opengis.net/eop/2.0", "eop20");
+                nvc.Set("http://www.opengis.net/opt/2.0", "opt20");
+                nvc.Set("http://www.opengis.net/alt/2.0", "alt20");
+                nvc.Set("http://www.opengis.net/gml", "gml");
+                nvc.Set("http://www.w3.org/2001/SMIL20/", "smil20");
+                nvc.Set("http://www.w3.org/2001/SMIL20/Language", "lang");
+                nvc.Set("http://www.isotc211.org/2005/gmd", "gmd");
+                nvc.Set("http://www.opengis.net/gml/3.2", "gml32");
+                nvc.Set("http://www.opengis.net/swe/1.0", "swe");
+                nvc.Set("http://www.w3.org/1999/xlink", "xlink");
+                nvc.Set("http://www.isotc211.org/2005/gco", "gco");
+                nvc.Set("http://www.opengis.net/om/2.0", "om");
+                nvc.Set("http://www.w3.org/2002/12/cal/ical#", "ical");
+                nvc.Set("http://www.opengis.net/ows/2.0", "ows");
+                nvc.Set("http://purl.org/dc/elements/1.1/", "dc");
+
+                return nvc;
+            }
+        }
+
+        public static XmlNamespaceManager GetXmlNamespaceManager(XElement elem) {
+            XmlNamespaceManager xnsm = new XmlNamespaceManager(elem.CreateReader().NameTable);
+            foreach (var key in TypeNamespaces.AllKeys) {
+                xnsm.AddNamespace(TypeNamespaces[key], key);
+            }
+
+            return xnsm;
+        }
+    }
+}
+
---
+++ Terradue.Metadata.EarthObservation/Terradue/Metadata/EarthObservation/OpenSearch/EarthObservationOpenSearchResultHelpers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Terradue.OpenSearch.Result;
+using System.Xml.XPath;
+using Terradue.ServiceModel.Syndication;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Terradue.Metadata.EarthObservation {
+    public static class EarthObservationOpenSearchResultHelpers {
+
+        public static void RestoreEnclosure(IOpenSearchResultItem item){
+
+            var matchLinks = item.Links.Where(l => l.RelationshipType == "enclosure").ToArray();
+            if (matchLinks.Count() == 0) {
+                foreach (var eo in item.ElementExtensions) {
+                    XElement eoElement = (XElement)XElement.ReadFrom(eo.GetReader());
+                    if (eoElement.Name.LocalName == "EarthObservation") {
+                        var result = eoElement.XPathSelectElement(string.Format("om:result/eop20:EarthObservationResult/eop20:product/eop20:ProductInformation", EONamespaces.TypeNamespaces[eo.OuterNamespace]), EONamespaces.GetXmlNamespaceManager(eoElement));
+                        if (result != null) {
+                            var link = result.XPathSelectElement("eop20:fileName/ows:ServiceReference", EONamespaces.GetXmlNamespaceManager(result));
+                            if (link != null && link.Attribute(XName.Get("href", "http://www.w3.org/1999/xlink")) != null) {
+                                long size = 0;
+                                var sizeElement = result.XPathSelectElement("eop20:size", EONamespaces.GetXmlNamespaceManager(result));
+                                if ( sizeElement != null )
+                                    long.TryParse(sizeElement.Value, out size);
+                                item.Links.Add(new SyndicationLink(new Uri(link.Attribute(XName.Get("href", "http://www.w3.org/1999/xlink")).Value), "enclosure", "Product file", "application/x-binary", size));
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public static void RestoreIdentifier(IOpenSearchResultItem item) {
+            var elements = item.ElementExtensions.ReadElementExtensions<string>("identifier", "http://purl.org/dc/elements/1.1/");
+            if (elements.Count == 0) {
+                foreach (var eo in item.ElementExtensions.ToList()) {
+                    XElement eoElement = (XElement)XElement.ReadFrom(eo.GetReader());
+                    if (eoElement.Name.LocalName == "EarthObservation") {
+                        var result = eoElement.XPathSelectElement(string.Format("eop20:metaDataProperty/eop20:EarthObservationMetaData/eop20:identifier", EONamespaces.TypeNamespaces[eo.OuterNamespace]), EONamespaces.GetXmlNamespaceManager(eoElement));
+                        if (result != null) {
+                            XElement identifier = new XElement(XName.Get("identifier", "http://purl.org/dc/elements/1.1/"), result.Value);
+                            item.ElementExtensions.Add(identifier.CreateReader());
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+}
+
