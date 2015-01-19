b12e0ff8a784161a05fcb63c066cba4b7f01769f
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2015-01-19 17:15:49 +0100

    Added test case for ngEO
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

---
+++ Terradue.Metadata.EarthObservation.Test/Test.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using Terradue.OpenSearch.Engine.Extensions;
+using Terradue.OpenSearch.Request;
+using Terradue.OpenSearch.Result;
+using System.IO;
+using System.Xml;
+using Terradue.ServiceModel.Syndication;
+using Terradue.OpenSearch;
+using System.Xml.Serialization;
+using Terradue.OpenSearch.Schema;
+using System.Linq;
+
+namespace Terradue.Metadata.EarthObservation.Test {
+
+    [TestFixture()]
+    public class Test {
+
+        [Test()]
+        public void TestCase() {
+
+            XmlReader responseReader = XmlReader.Create(new FileStream("../Samples/search_s2.xml", FileMode.Open, FileAccess.Read));
+            SyndicationFeed feed = SyndicationFeed.Load(responseReader);
+
+            AtomFeed afeed = new AtomFeed(feed);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(OpenSearchDescription));
+            XmlReader xmlReader = XmlReader.Create(new FileStream("../Samples/S2_OSDD.xml", FileMode.Open, FileAccess.Read));
+            OpenSearchDescription osd = (OpenSearchDescription)xmlSerializer.Deserialize(xmlReader);
+
+            var self = Terradue.Metadata.EarthObservation.EarthObservationOpenSearchResultHelpers.EntrySelfLinkTemplate(afeed.Items.Cast<AtomItem>().First(), osd, "application/atom+xml");
+
+            Assert.That(self.Contains("pgrpi="));
+
+        }
+    }
+}
+
