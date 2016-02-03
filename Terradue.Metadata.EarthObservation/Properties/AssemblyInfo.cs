3e8ee6e31481df2e24bac4b1d3bf1318fabbb776
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-02-03 15:29:58 +0100

    ready to pack 1.3.10

--- Terradue.Metadata.EarthObservation/Properties/AssemblyInfo.cs
+++ Terradue.Metadata.EarthObservation/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 @{
     Terradue .Net C# library implementing the Earth Observation Metadata profile of Observations & Measurements
 
-    \xrefitem sw_version "Versions" "Software Package Version" 1.3.9
+    \xrefitem sw_version "Versions" "Software Package Version" 1.3.10
 
     \xrefitem sw_link "Link" "Software Package Link" [DotNetEarthObservation](https://github.com/Terradue/DotNetEarthObservation)
 
@@ -37,8 +37,8 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.3.9.*")]
-[assembly: AssemblyInformationalVersion("1.3.9")]
+[assembly: AssemblyVersion("1.3.10.*")]
+[assembly: AssemblyInformationalVersion("1.3.10")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
