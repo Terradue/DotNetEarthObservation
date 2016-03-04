09fb82bb82381b24c0a0bad287a656cf904565f8
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-03-04 09:56:56 +0100

    nuget4mono

--- Terradue.Metadata.EarthObservation/Properties/AssemblyInfo.cs
+++ Terradue.Metadata.EarthObservation/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 @{
     Terradue .Net C# library implementing the Earth Observation Metadata profile of Observations & Measurements
 
-    \xrefitem sw_version "Versions" "Software Package Version" 1.3.4
+    \xrefitem sw_version "Versions" "Software Package Version" 1.4.0
 
     \xrefitem sw_link "Link" "Software Package Link" [DotNetEarthObservation](https://github.com/Terradue/DotNetEarthObservation)
 
@@ -26,6 +26,9 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
+using NuGet4Mono.Extensions;
+
+
 [assembly: AssemblyTitle("Terradue.Metadata.EarthObservation")]
 [assembly: AssemblyDescription(".Net C# library implementing the Earth Observation Metadata profile of Observations & Measurements")]
 [assembly: AssemblyConfiguration("")]
@@ -34,11 +37,11 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Terradue")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.3.7.*")]
-[assembly: AssemblyInformationalVersion("1.3.7")]
+[assembly: AssemblyAuthors("Emmanuel Mathot")]
+[assembly: AssemblyProjectUrl("https://github.com/Terradue/DotNetGeoJson")]
+[assembly: AssemblyLicenseUrl("https://github.com/Terradue/DotNetGeoJson/blob/master/LICENSE")]
+[assembly: AssemblyVersion("1.4.0.*")]
+[assembly: AssemblyInformationalVersion("1.4.0")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
