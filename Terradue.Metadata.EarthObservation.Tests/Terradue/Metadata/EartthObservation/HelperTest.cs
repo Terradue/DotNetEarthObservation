993892bcd1dd5e40df10012b6a128bfbda115ccf
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2020-01-28 13:03:28 +0100

    net472 + dotnet core

--- Terradue.Metadata.EarthObservation.Tests/Terradue/Metadata/EartthObservation/HelperTest.cs
+++ Terradue.Metadata.EarthObservation.Tests/Terradue/Metadata/EartthObservation/HelperTest.cs
@@ -48,13 +48,6 @@ namespace Terradue.Metadata.EarthObservation.Test
             Assert.AreEqual("disjoint", newParams["rel"]);
         }
 
-
-        [Test()]
-        public void Encoding()
-        {
-            Assembly.Load("I18N, Version=2.0.0.0, Culture=neutral, PublicKeyToken=0738eb9f132ed756");
-        }
-
         [Test()]
         public void CreateAtomItemFromEopProfile()
         {
